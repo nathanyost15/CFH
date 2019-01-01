02e4568fdc334a127760cb5d2251c66ce28cb35c
Nathaniel Yost <nathanyost15@gmail.com>
2019-01-01 15:21:46 -0500

    Black card get random question.

---
+++ Assets/Scripts/BlackCardTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackCardTest : MonoBehaviour
+{    
+    private Deck blackDeck;
+    private TMPro.TextMeshPro textBox;
+    void Start()
+    {        
+        textBox = (TMPro.TextMeshPro) GameObject.Find("BlackTextObj").GetComponent("TextMeshPro");        
+        if(textBox == null)
+        {
+            Debug.Log("Could not find game component Text Mesh");
+        }
+        blackDeck = new Deck("Assets/Docs/Black_Cards.txt");
+        if(blackDeck.GetError() != "")
+        {
+            Debug.Log("Could not find Black_Cards.txt");
+        }
+        blackDeck.Shuffle();
+        textBox.text = blackDeck.Draw();
+    }
+ 
+    void Update()
+    {        
+    }
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+
+public class Deck
+{
+    private string[] cards;
+    private string error;
+    private int deckIndex;
+    public Deck(string path)
+    {
+        deckIndex = 0;
+        error = "";
+        try
+        {
+            cards = System.IO.File.ReadAllLines(path);
+        }
+        catch (DirectoryNotFoundException e)
+        {
+            System.Console.WriteLine(e.Message);
+            error = e.Message;
+            return;
+        }
+    }
+
+    public String GetError()
+    {
+        return error;
+    }
+
+    public void Shuffle()
+    {
+        Random random = new Random();
+        for(int count = 0; count < cards.Length; count++)
+        {
+            int old = random.Next(0, cards.Length),
+                current = random.Next(0, cards.Length);
+            string temp = cards[old];
+            cards[old] = cards[current];
+            cards[current] = temp;
+        }
+        deckIndex = 0;
+    }
+
+    public string Draw()
+    {
+        string card = cards[deckIndex];
+        deckIndex = (deckIndex > cards.Length - 1) ? 0 : deckIndex + 1;        
+        return card;
+    }
+}
